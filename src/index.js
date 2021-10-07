24e93068a72724b2f4500599e25b5c49959d826d
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-07 18:49:08 +0700

    create function to readfile mode and interactive mode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-void main()
+#!/usr/bin/env node
+const fs = require('fs'),
+  readLine = require('readline');
+
+var commandLineInputs = process.argv, // processing command line inputs
+  interactiveMode = false;
+
+var processUserCommands = require('./controller/user_commands.js');
+
+
+// to avoid memory leaks errors, default max listeners = 10
+require('events').EventEmitter.defaultMaxListeners = 0;
+
+if (commandLineInputs[commandLineInputs.length - 1].endsWith('.txt')) {
+  interactiveMode = false;
+  fs.readFile(commandLineInputs[2], 'utf-8', function (err, data) {
+    if (err) {
+      console.log('Error in reading file');
+    }
+    var arr = data.split('\r\n');
+    for (var i = 0; i < arr.length; i++) {
+      processUserCommands(arr[i]);
+    }
+
+    // returning to console once all the inputs are processed
+    process.exit(1);
+  });
+}
+else {
+  interactiveMode = true;
+  openInteractiveConsole();
+}
+
+/**
+ * @description called when users want to interact via console
+ * it process one command at a time
+ */
+function openInteractiveConsole() {
+
+  var prompts = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+
+  // option for user to enter commands
+  if (interactiveMode) {
+    prompts.question('Input: ', function (data) {
+      processUserCommands(data);
+      openInteractiveConsole();
+    });
+  }
+}
+
