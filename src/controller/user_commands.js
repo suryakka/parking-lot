5283b21cdee27cfa38faf31a737494c3e0b75955
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-07 18:20:20 +0700

    create user_commands function

---
+++ src/controller/user_commands.js
@@ -0,0 +1,14 @@
+chalk = require('chalk');
+var Service = require('../services/parking_lot_service.js'),
+  parkingLotService = new Service();
+
+
+/**
+ *
+ * @param {String} input entered via console
+ * @description driver function for different commands for entered by users
+ * calls respective functions of ParkingLot class based on commands
+ */
+function processUserCommands(input){
+
+}
