a04171be488e5a20f7aee51420b0a3a72e151a89
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-07 18:14:11 +0700

    add service : parkCar

--- src/services/parking_lot_service.js
+++ src/services/parking_lot_service.js
@@ -3,7 +3,7 @@ var Car = require('../models/car.js');
 
 parkingLot = new Parking();
 
-class ParkingLotService{
+class ParkingLotService {
 
   /**
    *
@@ -21,6 +21,53 @@ class ParkingLotService{
       parkingLot.parkingSlots.push(null);
     }
     return parkingLot.MAX_PARKING_SLOTS;
+  }
+
+  /**
+   * @param {String} input user's input via terminal
+   * @description allocates nearest slot number to incoming cars.
+   * It throws an error if parking lot is empty or full.
+   * It also throws an error if only one field (either registration number or color) is provided.
+   */
+  parkCar(input) {
+    if (parkingLot.MAX_PARKING_SLOTS > 0) {
+      var car, carNumber;
+      if (this.isParkingSlotAvailable(parkingLot.parkingSlots) == true) {
+        carNumber = input.split(' ')[1];
+        if (parkingLot.parkingSlots.find(element => element?.NUMBER == carNumber)) {
+          throw new Error('Car with registration number ' + carNumber + ' is already parked');
+        }
+        var index = parkingLot.parkingSlots.findIndex(element => element == null);
+        if (index != null) {
+          car = new Car(carNumber);
+          parkingLot.parkingSlots[index] = car;
+          index = index + 1;
+          return index;
+        } else {
+          throw new Error('Please provide registration number');
+        }
+      }
+      else {
+        throw new Error('Sorry, parking lot is full');
+      }
+    }
+    else {
+      throw new Error('Minimum one slot is required to create parking slot');
+    }
+  }
+
+
+  /**
+   * @description check parking slot is available or not.
+   */
+  isParkingSlotAvailable() {
+    var isAvailable = false;
+    for (var i = 0; i < parkingLot.parkingSlots.length; i++) {
+      if (parkingLot.parkingSlots[i] == null) {
+        isAvailable = true;
+      }
+    }
+    return isAvailable;
   }
 
 }
