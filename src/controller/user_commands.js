f9df682a797153ef402b78ee5c4208e6c32459bc
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-07 18:42:17 +0700

    add command case : create_parking_lot

--- src/controller/user_commands.js
+++ src/controller/user_commands.js
@@ -9,6 +9,29 @@ var Service = require('../services/parking_lot_service.js'),
  * @description driver function for different commands for entered by users
  * calls respective functions of ParkingLot class based on commands
  */
-function processUserCommands(input){
+function processUserCommands(input) {
+  var userCommand = input.split(' ')[0],
+    totalParkingSlots,
+    parkingSlotNumber,
+    parkingCharge;
+  switch (userCommand) {
+    case 'create_parking_lot':
+      try {
+        totalParkingSlots = parkingLotService.createParkingLot(input);
+        console.log(chalk.yellow.bold('Created parking lot with ' + totalParkingSlots + ' slots.'));
+      }
+      catch (err) {
+        console.log(chalk.red.bold(err.message));
+      }
+      break;
 
+    case 'exit':
+      process.exit(0);
+    default:
+      console.log(chalk.red.bold(input, 'is an invalid command'));
+      break;
+  }
+
+  console.log();
 }
+module.exports = processUserCommands;
