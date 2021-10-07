721068c48bd405f680f9654584dda0e5181ab20d
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-07 23:56:52 +0700

    rename parking slot

---
+++ src/models/parking_slot.js
@@ -0,0 +1,8 @@
+class ParkingSlot {
+  constructor() {
+    this.MAX_PARKING_SLOTS = 0; // maximum parking slots allowed
+    this.parkingSlots = new Array(); // array for parking slots
+  }
+
+}
+module.exports = ParkingSlot;
