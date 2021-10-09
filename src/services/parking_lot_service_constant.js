382bbc261dc641f1bb1c683b875a7061ff549f11
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-09 19:17:39 +0700

    create parking lot constant

---
+++ src/services/parking_lot_service_constant.js
@@ -0,0 +1,14 @@
+const ERROR_SERVICE = {
+  MINIMUM_ONE_SLOT: 'Minimum one slot is required to create parking slot',
+  PLEASE_PROVIDE_REGIS_NUMBER: 'Please provide registration number',
+  CAR_WITH_REGIS_NUMBER: 'Car with registration number ',
+  IS_ALREADY_PARKED: ' is already parked',
+  PARKING_LOT_FULL: 'Sorry, parking lot is full',
+  NO_PARKING_LOT_CREATED: 'Sorry, the parking lot has not been created',
+  REGISTRATION_NUMBER: 'Registration number ',
+  NOT_FOUND: ' not found',
+  PLEASE_PROVIDE_PARKING_DURATION: 'Please provide parking duration',
+  SLOT_NO_REGIS_NO: 'Slot No. Registration.'
+}
+
+module.exports = ERROR_SERVICE;
