9e9415d2d2281eaa96739b53e88b321271f961cc
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-13 14:44:32 +0700

    refactor parking lot service test

--- src/services/__test__/parking_lot_service.spec.js
+++ src/services/__test__/parking_lot_service.spec.js
@@ -1,42 +1,29 @@
 
-const ParkingLotService = require('../../services/parking_lot_service.js'), fs = require('fs');
+const ParkingLot = require('../../models/parking_lot.js');
+const ParkingLotService = require('../../services/parking_lot_service.js');
+var parkingLot = new ParkingLot();
 require('jest');
 
-var commands = [],
-  totalParkings,
-  parkingLot = new ParkingLotService();
+var
+  parkingLotService = new ParkingLotService(parkingLot);
 
-
-test('park car failed because no parking lot test', () => {
-  expect(() => parkingLot.parkCar('park II')).toThrow('Sorry, the parking lot has not been created');
+beforeEach(() => {
+  parkingLot.MAX_PARKING_SLOTS = 0;
+  parkingLot.parkingSlots = [];
 });
 
-test('leave car failed because no parking lot test', () => {
-  expect(() => parkingLot.leave('leave II')).toThrow('Sorry, the parking lot has not been created');
+test('get parking status test', () => {
+  var status = parkingLotService.isParkingSlotAvailable(parkingLot);
+  expect(status).toEqual(null);
 });
 
 test('create parking lot test', () => {
-  totalParkings = parkingLot.createParkingLot('create_parking_lot 6');
-  expect(totalParkings).toEqual(6);
+  var totalParkings = parkingLotService.createParkingLot(6);
+  expect(totalParkings).toBe(6);
 });
 
-test('park car full test', () => {
-  expect(() => {
-    parkingLot.createParkingLot('create_parking_lot 1');
-    parkingLot.parkCar('park HH');
-    parkingLot.parkCar('park II');
-  }).toThrow('Sorry, parking lot is full');
-});
-
-test('leave car not found test', () => {
-  expect(() => {
-    parkingLot.createParkingLot('create_parking_lot 2');
-    parkingLot.leave('leave XX 3');
-  }).toThrow('Registration number XX not found');
-});
-
-test('leave car not found test', () => {
-  expect(() => {
-    parkingLot.leave('leave');
-  }).toThrow('Please provide registration number');
+test('park car test', () => {
+parkingLotService.createParkingLot(6);
+  var status = parkingLotService.parkCar('KK-HH');
+  expect(status).toEqual(1);
 });
