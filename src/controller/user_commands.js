75e6270fb0150bf5f21600bd9ea303abed7968e8
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-13 14:45:38 +0700

    implement user commands

--- src/controller/user_commands.js
+++ src/controller/user_commands.js
@@ -1,9 +1,7 @@
 const { COMMAND, COMMAND_LOG } = require('./user_commands_constant.js');
 
 const chalk = require('chalk');
-var Service = require('../services/parking_lot_service.js'),
-  parkingLotService = new Service();
-
+var ParkingLotController = require('./parking_lot_controller.js'), controller = new ParkingLotController();
 
 /**
  *
@@ -12,56 +10,27 @@ var Service = require('../services/parking_lot_service.js'),
  * calls respective functions of ParkingLotService based on commands
  */
 function processUserCommands(input) {
-  var userCommand = input.split(' ')[0],
-    totalParkingSlots,
-    parkingSlotNumber,
-    parkingCharge;
+  var userCommand = input.split(' ')[0];
   switch (userCommand) {
     case COMMAND.CREATE_PARKING_LOT:
-      try {
-        totalParkingSlots = parkingLotService.createParkingLot(input);
-        console.log(chalk.yellow.bold(COMMAND_LOG.CREATED_PARKING_LOT + totalParkingSlots + COMMAND_LOG.SLOTS));
-      }
-      catch (err) {
-        console.log(chalk.red.bold(err.message));
-      }
+      controller.createParkingLot(input);
       break;
 
     case COMMAND.PARK:
-      try {
-        parkingSlotNumber = parkingLotService.parkCar(input);
-        console.log(chalk.green(COMMAND_LOG.ALLOCATED_SLOT + parkingSlotNumber));
-      }
-      catch (err) {
-        console.log(chalk.red.bold(err.message));
-      }
+      controller.park(input);
       break;
 
     case COMMAND.LEAVE:
-      try {
-        parkingSlotNumber = parkingLotService.leave(input);
-        parkingCharge = parkingLotService.getParkingCharge(input);
-        var registrationNumber = input.split(' ')[1];
-        console.log(chalk.blue(COMMAND_LOG.REGISTATION_NUMBER + registrationNumber + COMMAND_LOG.WITH_SLOT_NUMBER + parkingSlotNumber + COMMAND_LOG.IS_FREE_WITH_CHARGE + parkingCharge));
-
-      }
-      catch (err) {
-        console.log(chalk.red.bold(err.message)); // handling exceptions
-      }
+      controller.leave(input);
       break;
 
     case COMMAND.STATUS:
-      try {
-        var parkingSlotStatus = parkingLotService.getParkingStatus();
-        console.log(parkingSlotStatus.join('\n'));
-      }
-      catch (err) {
-        console.log(chalk.red.bold(err.message));
-      }
+      controller.status();
       break;
 
     case COMMAND.EXIT:
       return 0;
+
     default:
       console.log(chalk.red.bold(input + COMMAND_LOG.INVALID_COMMAND));
       break;
